7a9a40573664d437d4ab5f50c00cdef09d210a41
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-01-24 21:24:08 +0100

    playing with multi layered network

--- index.py
+++ index.py
@@ -6,7 +6,9 @@ import csv
 
 print("Hello world")
 
-
+# Network structure
+# inputs outputs
+# 4      3        
 inputs = [1.0, 2.0, 3.0, 2.5]
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
@@ -19,25 +21,41 @@ print(output)
 
 # Network structure
 # inputs hidden layers outputs
-# 3      [ 4 ]         1
+# 4      [ 3 3 ]       1
 
-"""
+
 inputs = [[1, 2, 3, 2.5],
           [2.0, 5.0, -1.0, 2.0],
           [-1.5, 2.7, 3.3, 0.8]]
 
 weights = [[0.2, 0.8, -0.5, 1.0],
           [0.5, -0.91, 0.26, -0.5],
-          [-0.26, -0,27, 0.17, 0.87]]
+          [-0.26, -0.27, 0.17, 0.87]]
 
 biases = [2, 3, 0.5]
+
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, 0.5]
 
 # output = input[0] * weights[0] + input[1] * weights[1]
 
 # input[0] * weights[0] + input[1] * weights[1] + ... + bias
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
-"""
+layer1Outputs = np.dot(inputs, np.array(weights).transpose()) + biases
+layer2Outputs = np.dot(layer1Outputs, np.array(weights2).transpose()) + biases2
+print(layer2Outputs)
+
+
+# Network structure
+# inputs hidden layers outputs
+# 5      [ 4 4 ]       3
+
+inputs = [1]
+hidden1 = []
+hidden2 = []
+
 
 
 # Network structure
@@ -51,7 +69,7 @@ def backPropagate():
   return
 
 def activationFunction(x):
-  return np.maximum(0, x) # Relu formula y
+  return max(0, x) # Relu formula y
 
 """
 with open('dataset/mnist_train_min.csv', newline='') as file:
